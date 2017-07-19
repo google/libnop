f7a482b2ab2335fbe544d1ee4243c8e7732b0dfd
Corey Tabaka <eieio@google.com>
2017-07-19 16:24:29 -0700

    Add fd stream support to test stream utilities.

--- examples/stream_utilities.h
+++ examples/stream_utilities.h
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <vector>
 
+#include <unistd.h>
+
 namespace {
 
 // Prints a std::vector<T> to the given stream. This template will work for any
@@ -48,6 +50,94 @@ std::ostream& operator<<(std::ostream& stream,
   stream << "}";
   return stream;
 }
+
+class FdOutputBuffer : public std::streambuf {
+ public:
+  FdOutputBuffer(int fd) : fd_{fd} {}
+  ~FdOutputBuffer() { close(fd_); }
+
+  int_type overflow(int_type c) override {
+    if (c != EOF) {
+      char value = c;
+      if (write(fd_, &value, sizeof(value)) != 1)
+        return EOF;
+    }
+    return c;
+  }
+
+  std::streamsize xsputn(const char* data, std::streamsize size) override {
+    return write(fd_, data, size);
+  }
+
+ private:
+  int fd_;
+
+  FdOutputBuffer(const FdOutputBuffer&) = delete;
+  void operator=(const FdOutputBuffer&) = delete;
+};
+
+class FdOutputStream : public std::ostream {
+ public:
+  FdOutputStream(int fd) : std::ostream{0}, buffer_{fd} { rdbuf(&buffer_); }
+
+ private:
+  FdOutputBuffer buffer_;
+
+  FdOutputStream(const FdOutputStream&) = delete;
+  void operator=(const FdOutputStream&) = delete;
+};
+
+class FdInputBuffer : public std::streambuf {
+ public:
+  FdInputBuffer(int fd) : fd_{fd} {
+    setg(&buffer_[4], &buffer_[4], &buffer_[4]);
+  }
+  ~FdInputBuffer() { close(fd_); }
+
+  int_type underflow() override {
+    // Is read position before end of buffer?
+    if (gptr() < egptr()) {
+      return traits_type::to_int_type(*gptr());
+    }
+
+    int num_putback;
+    num_putback = gptr() - eback();
+    if (num_putback > 4)
+      num_putback = 4;
+
+    std::memmove(&buffer_[4 - num_putback], gptr() - num_putback, num_putback);
+
+    const int count = read(fd_, &buffer_[4], kBufferSize - 4);
+    if (count <= 0)
+      return EOF;
+
+    setg(&buffer_[4 - num_putback], &buffer_[4], &buffer_[4 + count]);
+
+    return traits_type::to_int_type(*gptr());
+  }
+
+ private:
+  enum : std::size_t { kBufferSize = 10 };
+
+  int fd_;
+  char buffer_[kBufferSize];
+
+  FdInputBuffer(const FdInputBuffer&) = delete;
+  void operator=(const FdInputBuffer&) = delete;
+};
+
+class FdInputStream : public std::istream {
+ public:
+  FdInputStream(int fd) : std::istream{nullptr}, buffer_{fd} {
+    rdbuf(&buffer_);
+  }
+
+ private:
+  FdInputBuffer buffer_;
+
+  FdInputStream(const FdInputStream&) = delete;
+  void operator=(const FdInputStream&) = delete;
+};
 
 }  // anonymous namespace
 
