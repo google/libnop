44d3516f07b5352b73f32ee3c47f8241927eed38
Corey Tabaka <eieio@google.com>
2017-07-23 20:34:55 -0700

    Clean up std::string encoding and use correct expression for write length.

--- include/nop/base/string.h
+++ include/nop/base/string.h
@@ -19,13 +19,12 @@ template <>
 struct Encoding<std::string> : EncodingIO<std::string> {
   using Type = std::string;
 
-  static constexpr EncodingByte Prefix(const std::string& /*value*/) {
+  static constexpr EncodingByte Prefix(const Type& /*value*/) {
     return EncodingByte::String;
   }
 
-  static constexpr std::size_t Size(const std::string& value) {
-    const std::size_t length_bytes =
-        value.length() * sizeof(std::string::value_type);
+  static constexpr std::size_t Size(const Type& value) {
+    const std::size_t length_bytes = value.length() * sizeof(Type::value_type);
     return BaseEncodingSize(Prefix(value)) +
            Encoding<std::uint64_t>::Size(length_bytes) + length_bytes;
   }
@@ -35,9 +34,10 @@ struct Encoding<std::string> : EncodingIO<std::string> {
   }
 
   template <typename Writer>
-  static Status<void> WritePayload(EncodingByte /*prefix*/,
-                                   const std::string& value, Writer* writer) {
-    auto status = Encoding<std::uint64_t>::Write(value.length(), writer);
+  static Status<void> WritePayload(EncodingByte /*prefix*/, const Type& value,
+                                   Writer* writer) {
+    const std::size_t length_bytes = value.length() * sizeof(Type::value_type);
+    auto status = Encoding<std::uint64_t>::Write(length_bytes, writer);
     if (!status)
       return status;
 
@@ -45,16 +45,16 @@ struct Encoding<std::string> : EncodingIO<std::string> {
   }
 
   template <typename Reader>
-  static Status<void> ReadPayload(EncodingByte /*prefix*/, std::string* value,
+  static Status<void> ReadPayload(EncodingByte /*prefix*/, Type* value,
                                   Reader* reader) {
     std::uint64_t length_bytes;
     auto status = Encoding<std::uint64_t>::Read(&length_bytes, reader);
     if (!status)
       return status;
-    else if (length_bytes % sizeof(std::string::value_type) != 0)
+    else if (length_bytes % sizeof(Type::value_type) != 0)
       return ErrorStatus(EPROTO);
 
-    const std::uint64_t size = length_bytes / sizeof(std::string::value_type);
+    const std::uint64_t size = length_bytes / sizeof(Type::value_type);
 
     // Make sure the reader has enough data to fulfill the requested size as a
     // defense against abusive or erroneous string sizes.
