3b362b3116fb0a753708e2ba40ebf1b73f6a7133
Corey Tabaka <eieio@google.com>
2017-12-20 18:26:39 -0800

    Enhance macro list processing to support new grouping requirements.

--- include/nop/base/macros.h
+++ include/nop/base/macros.h
@@ -2,14 +2,6 @@
 #define LIBNOP_INCLUDE_NOP_BASE_MACROS_H_
 
 // Macros to apply other macros over all elements in a list.
-//
-// For example, for a macro A(x) and B(x, y):
-// - FOR_EACH(A, 1, 2, 3) -> A(1) A(2) A(3).
-// - FOR_EACH_BINARY(B, z, 1, 2, 3) -> B(z, 1) B(z, 2) B(z, 3)
-// - FOR_EACH_LIST(A, 1, 2, 3) -> A(1), B(2), C(3)
-// - FOR_EACH_BINARY_LIST(B, z, 1, 2, 3) -> B(z, 1), B(z, 2), B(z, 3)
-//
-// Empty lists are supported and will produce no output.
 
 // Recursive expansion macros.
 #define _NOP_EXPAND0(...) __VA_ARGS__
@@ -17,7 +9,8 @@
 #define _NOP_EXPAND2(...) _NOP_EXPAND1(_NOP_EXPAND1(_NOP_EXPAND1(__VA_ARGS__)))
 #define _NOP_EXPAND3(...) _NOP_EXPAND2(_NOP_EXPAND2(_NOP_EXPAND2(__VA_ARGS__)))
 #define _NOP_EXPAND4(...) _NOP_EXPAND3(_NOP_EXPAND3(_NOP_EXPAND3(__VA_ARGS__)))
-#define _NOP_EXPAND(...) _NOP_EXPAND4(_NOP_EXPAND4(_NOP_EXPAND4(__VA_ARGS__)))
+#define _NOP_EXPAND5(...) _NOP_EXPAND4(_NOP_EXPAND4(_NOP_EXPAND4(__VA_ARGS__)))
+#define _NOP_EXPAND(...)  _NOP_EXPAND5(_NOP_EXPAND5(_NOP_EXPAND5(__VA_ARGS__)))
 
 // Required to workaround a bug in the VC++ preprocessor.
 #define _NOP_INDIRECT_EXPAND(macro, args) macro args
@@ -34,115 +27,84 @@
 // Returns the first argument of a list.
 #define _NOP_FIRST_ARG(first, ...) first
 
+#define _NOP_REST_ARG(_, ...) __VA_ARGS__
+
 // Returns the second argument of a list.
 #define _NOP_SECOND_ARG(_, second, ...) second
 
-// Expands the arguments and introduces a separator.
-#define _NOP_EXPAND_NEXT_FUNC(_, next_func, ...)        \
-  _NOP_INDIRECT_EXPAND(_NOP_SECOND_ARG, (_, next_func)) \
-  _NOP_SEPARATOR
+#define _NOP_CAT(a, ...) a##__VA_ARGS__
 
-// Returns next_func if the next element is not (), or _NOP_CLEAR
-// otherwise.
-//
-// _NOP_CLEAR_IF_LAST inserts an extra first dummy argument if peek is ().
-#define _NOP_NEXT_FUNC(next_element, next_func) \
-  _NOP_EXPAND_NEXT_FUNC(_NOP_CLEAR_IF_LAST next_element, next_func)
+#define _NOP_IS_PROBE(...) _NOP_SECOND_ARG(__VA_ARGS__, 0)
+#define _NOP_PROBE() ~, 1
 
-// Macros for the unary version of NOP_FOR_EACH.
+#define _NOP_IS_PAREN(...) _NOP_IS_PROBE(_NOP_IS_PAREN_PROBE __VA_ARGS__)
+#define _NOP_IS_PAREN_PROBE(...) _NOP_PROBE()
 
-// Applies the unary macro. Duplicated for macro recursive expansion.
-#define _NOP_APPLY_1(macro, head, next, ...) \
-  macro(head) _NOP_NEXT_FUNC(next, _NOP_APPLY_2)(macro, next, __VA_ARGS__)
+#define _NOP_NOT(x) _NOP_IS_PROBE(_NOP_CAT(_NOP_NOT_, x))
+#define _NOP_NOT_0 _NOP_PROBE()
 
-// Applies the unary macro. Duplicated for macro recursive expansion.
-#define _NOP_APPLY_2(macro, head, next, ...) \
-  macro(head) _NOP_NEXT_FUNC(next, _NOP_APPLY_1)(macro, next, __VA_ARGS__)
+#define _NOP_BOOL(x) _NOP_NOT(_NOP_NOT(x))
 
-// Stops expansion if __VA_ARGS__ is empty, calling _NOP_APPLY_1
-// otherwise.
-#define _NOP_HANDLE_EMPTY_ARGS(macro, ...)                    \
-  _NOP_NEXT_FUNC(_NOP_FIRST_ARG(__VA_ARGS__()), _NOP_APPLY_1) \
-  (macro, __VA_ARGS__, ())
+#define _NOP_IF_ELSE(condition) __NOP_IF_ELSE(_NOP_BOOL(condition))
+#define __NOP_IF_ELSE(condition) _NOP_CAT(_NOP_IF_, condition)
 
-// Applies a unary macro over all the elements in a list.
-#define NOP_FOR_EACH(macro, ...) \
-  _NOP_EXPAND(_NOP_HANDLE_EMPTY_ARGS(macro, __VA_ARGS__))
+#define _NOP_IF_1(...) __VA_ARGS__ _NOP_IF_1_ELSE
+#define _NOP_IF_0(...) _NOP_IF_0_ELSE
 
-// Applies the unary macro at the end of a list. Duplicated for macro recursive
-// expansion.
-#define _NOP_APPLY_LIST_1(macro, head, next, ...) \
-  , macro(head)                                   \
-        _NOP_NEXT_FUNC(next, _NOP_APPLY_LIST_2)(macro, next, __VA_ARGS__)
+#define _NOP_IF_1_ELSE(...)
+#define _NOP_IF_0_ELSE(...) __VA_ARGS__
 
-// Applies the unary macro at the end of a list. Duplicated for macro recursive
-// expansion.
-#define _NOP_APPLY_LIST_2(macro, head, next, ...) \
-  , macro(head)                                   \
-        _NOP_NEXT_FUNC(next, _NOP_APPLY_LIST_1)(macro, next, __VA_ARGS__)
+#define _NOP_HAS_ARGS(...) \
+  _NOP_BOOL(_NOP_FIRST_ARG(_NOP_END_OF_ARGUMENTS_ __VA_ARGS__)())
+#define _NOP_END_OF_ARGUMENTS_(...) _NOP_IS_PAREN(__VA_ARGS__)
 
-// Applies the unary macro at the start of a list.
-#define _NOP_APPLY_LIST_0(macro, head, next, ...) \
-  macro(head) _NOP_NEXT_FUNC(next, _NOP_APPLY_LIST_1)(macro, next, __VA_ARGS__)
+#define _NOP_EMPTY()
+#define _NOP_DEFER1(m) m _NOP_EMPTY()
+#define _NOP_DEFER2(m) m _NOP_EMPTY _NOP_EMPTY()()
+#define _NOP_DEFER3(m) m _NOP_EMPTY _NOP_EMPTY _NOP_EMPTY()()()
+#define _NOP_DEFER4(m) m _NOP_EMPTY _NOP_EMPTY _NOP_EMPTY _NOP_EMPTY()()()()
+#define _NOP_DEFER5(m) \
+  m _NOP_EMPTY _NOP_EMPTY _NOP_EMPTY _NOP_EMPTY _NOP_EMPTY()()()()()
 
-// Stops expansion if __VA_ARGS__ is empty, calling _NOP_APPLY_LIST_0
-// otherwise.
-#define _NOP_HANDLE_EMPTY_LIST(macro, ...)                         \
-  _NOP_NEXT_FUNC(_NOP_FIRST_ARG(__VA_ARGS__()), _NOP_APPLY_LIST_0) \
-  (macro, __VA_ARGS__, ())
+#define _NOP_REMOVE_PARENS(...)            \
+  _NOP_IF_ELSE(_NOP_IS_PAREN(__VA_ARGS__)) \
+    (_NOP_STRIP_PARENS __VA_ARGS__)        \
+    (__VA_ARGS__)
 
-// Applies a unary macro over all the elements in a list.
-#define NOP_FOR_EACH_LIST(macro, ...) \
-  _NOP_EXPAND(_NOP_HANDLE_EMPTY_LIST(macro, __VA_ARGS__))
+#define _NOP_STRIP_PARENS(...) __VA_ARGS__
 
-// Macros for the binary version of NOP_FOR_EACH.
+#define NOP_MAP(...) _NOP_EXPAND(_NOP_MAP_FIRST(__VA_ARGS__))
 
-// Applies the binary macro. Duplicated for macro recursive expansion.
-#define _NOP_APPLY_BINARY_1(macro, arg, head, next, ...) \
-  macro(arg, head)                                       \
-      _NOP_NEXT_FUNC(next, _NOP_APPLY_BINARY_2)(macro, arg, next, __VA_ARGS__)
+#define _NOP_MAP_FIRST(m, ...)                         \
+  _NOP_IF_ELSE(_NOP_HAS_ARGS(__VA_ARGS__)) (           \
+    m(_NOP_REMOVE_PARENS(_NOP_FIRST_ARG(__VA_ARGS__))) \
+    _NOP_MAP_NEXT(m, _NOP_REST_ARG(__VA_ARGS__))       \
+  )(/* done */)
 
-// Applies the binary macro. Duplicated for macro recursive expansion.
-#define _NOP_APPLY_BINARY_2(macro, arg, head, next, ...) \
-  macro(arg, head)                                       \
-      _NOP_NEXT_FUNC(next, _NOP_APPLY_BINARY_1)(macro, arg, next, __VA_ARGS__)
+#define _NOP_MAP_NEXT(m, ...)                                    \
+  _NOP_IF_ELSE(_NOP_HAS_ARGS(__VA_ARGS__)) (                     \
+    , m(_NOP_REMOVE_PARENS(_NOP_FIRST_ARG(__VA_ARGS__)))         \
+    _NOP_DEFER3(__NOP_MAP_NEXT)()(m, _NOP_REST_ARG(__VA_ARGS__)) \
+  )(/* done */)
 
-// Version of _NOP_HANDLE_EMPTY_ARGS that takes 1 fixed argument for a
-// binary macro.
-#define _NOP_HANDLE_EMPTY_ARGS_BINARY(macro, arg, ...)               \
-  _NOP_NEXT_FUNC(_NOP_FIRST_ARG(__VA_ARGS__()), _NOP_APPLY_BINARY_1) \
-  (macro, arg, __VA_ARGS__, ())
+#define __NOP_MAP_NEXT() _NOP_MAP_NEXT
 
-// Applies a binary macro over all the elements in a list and a given argument.
-#define NOP_FOR_EACH_BINARY(macro, arg, ...) \
-  _NOP_EXPAND(_NOP_HANDLE_EMPTY_ARGS_BINARY(macro, arg, __VA_ARGS__))
+#define NOP_MAP_ARGS(...) _NOP_EXPAND(_NOP_MAP_FIRST_ARGS(__VA_ARGS__))
+#define _NOP_MAP_ARGS() NOP_MAP_ARGS
 
-// Applies the binary macro at the end of a list. Duplicated for macro recursive
-// expansion.
-#define _NOP_APPLY_BINARY_LIST_1(macro, arg, head, next, ...)        \
-  , macro(arg, head) _NOP_NEXT_FUNC(next, _NOP_APPLY_BINARY_LIST_2)( \
-        macro, arg, next, __VA_ARGS__)
+#define _NOP_MAP_FIRST_ARGS(m, args, ...)                                        \
+  _NOP_IF_ELSE(_NOP_HAS_ARGS(__VA_ARGS__)) (                                     \
+    m(_NOP_REMOVE_PARENS(args), _NOP_REMOVE_PARENS(_NOP_FIRST_ARG(__VA_ARGS__))) \
+    _NOP_MAP_NEXT_ARGS(m, args, _NOP_REST_ARG(__VA_ARGS__))                      \
+  )(/* done */)
 
-// Applies the binary macro at the end of a list. Duplicated for macro recursive
-// expansion.
-#define _NOP_APPLY_BINARY_LIST_2(macro, arg, head, next, ...)        \
-  , macro(arg, head) _NOP_NEXT_FUNC(next, _NOP_APPLY_BINARY_LIST_1)( \
-        macro, arg, next, __VA_ARGS__)
+#define _NOP_MAP_NEXT_ARGS(m, args, ...)                                           \
+  _NOP_IF_ELSE(_NOP_HAS_ARGS(__VA_ARGS__)) (                                       \
+    , m(_NOP_REMOVE_PARENS(args), _NOP_REMOVE_PARENS(_NOP_FIRST_ARG(__VA_ARGS__))) \
+    _NOP_DEFER3(__NOP_MAP_NEXT_ARGS)()(m, args, _NOP_REST_ARG(__VA_ARGS__))        \
+  )(/* done */)
 
-// Applies the binary macro at the start of a list. Duplicated for macro
-// recursive expansion.
-#define _NOP_APPLY_BINARY_LIST_0(macro, arg, head, next, ...)      \
-  macro(arg, head) _NOP_NEXT_FUNC(next, _NOP_APPLY_BINARY_LIST_1)( \
-      macro, arg, next, __VA_ARGS__)
+#define __NOP_MAP_NEXT_ARGS() _NOP_MAP_NEXT_ARGS
 
-// Version of _NOP_HANDLE_EMPTY_LIST that takes 1 fixed argument for a
-// binary macro.
-#define _NOP_HANDLE_EMPTY_LIST_BINARY(macro, arg, ...)                    \
-  _NOP_NEXT_FUNC(_NOP_FIRST_ARG(__VA_ARGS__()), _NOP_APPLY_BINARY_LIST_0) \
-  (macro, arg, __VA_ARGS__, ())
-
-// Applies a binary macro over all the elements in a list and a given argument.
-#define NOP_FOR_EACH_BINARY_LIST(macro, arg, ...) \
-  _NOP_EXPAND(_NOP_HANDLE_EMPTY_LIST_BINARY(macro, arg, __VA_ARGS__))
 
 #endif  // LIBNOP_INCLUDE_NOP_BASE_MACROS_H_
